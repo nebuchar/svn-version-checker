9d9c0902a0a30e14b14f4c731db9a71f51f4f9ab
Albert Paris <albert.paris@ext.privalia.com>
2015-11-25 16:30:10 +0100

    Options

--- background.js
+++ background.js
@@ -51,4 +51,4 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 
 // Listen for any changes to the URL of any tab.
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
-chrome.pageAction.onClicked.addListener(checkForValidUrl);
+chrome.pageAction.onClicked.addListener(getSvnInfo);
